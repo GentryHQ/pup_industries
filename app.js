b639acd3534a6613ae142168e6433b3f6adf803a
GentryHQ <gentryweb1gmail.com>
2023-06-18 18:39:53 +0100

    mobile nav bar working

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('works');
+const menuToggle = document.querySelector('.menuToggle');
+const menu = document.querySelector('.nav_links');
+
+
+menuToggle.addEventListener('click', ()=>{
+    console.log('works');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+})
